ba3346cd001901a1f3c9a0ab01145b7b2e74b6b4
SAC <79229673+anonymous-sac@users.noreply.github.com>
2021-02-20 20:57:17 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Leave Home Play Safe Bundle
+  
+## 📄 About 
+**LeaveHomePlaySafeBundle** is a bundle of [LeaveHomeSafe](https://www.leavehomesafe.gov.hk/en/)'s PWA clone for avoiding HK government tracking. Here's the [showcase](https://leave-home-play-safe.000webhostapp.com).
+
+
+
+
+## 💰 Donation
+* BTC: bc1qxecefqvcghf4m2arynpwgufuwegkkvqjvglt9j
+* ETH: 0x405A2E1e51E8fd0FF698220Ed1AC6D35BD2A6EEA
---
+++ precache-manifest.1195ed7b481c4a5efc400d2fc0747cf2.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86272a341757db0849d649361ca1da4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51a9f234f4ba6575f554",
+    "url": "/static/css/2.bf6e87a0.chunk.css"
+  },
+  {
+    "revision": "60d2e69e1037e39a154f",
+    "url": "/static/css/3.6b292308.chunk.css"
+  },
+  {
+    "revision": "d6092af047e9e9d97573",
+    "url": "/static/css/4.d505494c.chunk.css"
+  },
+  {
+    "revision": "3327816f5770c1742d14",
+    "url": "/static/css/main.cf81a4dc.chunk.css"
+  },
+  {
+    "revision": "51a9f234f4ba6575f554",
+    "url": "/static/js/2.11dc0448.chunk.js"
+  },
+  {
+    "revision": "8bef6c4ac161bfe3669963955f843776",
+    "url": "/static/js/2.11dc0448.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60d2e69e1037e39a154f",
+    "url": "/static/js/3.7a6303a0.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/3.7a6303a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6092af047e9e9d97573",
+    "url": "/static/js/4.8e2c11ad.chunk.js"
+  },
+  {
+    "revision": "3327816f5770c1742d14",
+    "url": "/static/js/main.030166ce.chunk.js"
+  },
+  {
+    "revision": "25e625c85e6867569442",
+    "url": "/static/js/runtime-main.0c5bf7d6.js"
+  },
+  {
+    "revision": "ca0595d3eaf20c09083843b55fbe93b8",
+    "url": "/static/media/1.ca0595d3.jpg"
+  },
+  {
+    "revision": "476672f721035760adedd4a02a5b016a",
+    "url": "/static/media/2.476672f7.jpg"
+  },
+  {
+    "revision": "83ec76001842f7197d71538c495f5439",
+    "url": "/static/media/3.83ec7600.jpg"
+  },
+  {
+    "revision": "6eeae7eb633d442260785eb4314db1a9",
+    "url": "/static/media/4.6eeae7eb.jpg"
+  },
+  {
+    "revision": "7415dd3fd73575843c60949ae6fde587",
+    "url": "/static/media/filter.7415dd3f.png"
+  },
+  {
+    "revision": "03575adb0f637f057d2ff972b6157e75",
+    "url": "/static/media/leave.03575adb.svg"
+  },
+  {
+    "revision": "074223859841ea6a0057fb8cb4ab766d",
+    "url": "/static/media/report_zh-cn.07422385.jpg"
+  },
+  {
+    "revision": "28ec4ec4c8514ed4279e4007c829a3e3",
+    "url": "/static/media/report_zh-hk.28ec4ec4.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "242f0d165241cec0992b52930be673e1",
+    "url": "/static/media/stay_zh-cn.242f0d16.jpg"
+  },
+  {
+    "revision": "b5ef3198bf59c51b3111e438d76bdf08",
+    "url": "/static/media/stay_zh-hk.b5ef3198.jpg"
+  },
+  {
+    "revision": "01a27e2049852a45daa26efce502cda3",
+    "url": "/static/media/transport_zh-cn.01a27e20.jpg"
+  },
+  {
+    "revision": "efc53a1871b8095ad16753f1e149c6a7",
+    "url": "/static/media/transport_zh-hk.efc53a18.jpg"
+  }
+]);
